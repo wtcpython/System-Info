e05d08b056b807bfcfef92cdedd15b8c6e5e65df
wtcpython <1762993226@qq.com>
2025-08-04 21:20:59 +0800

    版本更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 31
         targetSdk = 35
         versionCode = 2
-        versionName = "1.1"
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
